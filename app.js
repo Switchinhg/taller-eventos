f1a9e80f9358609977d0c0a28381aa8bb6e01b08
swicho <santifonlop@hotmail.com>
2023-08-15 14:20:50 -0300

    solucion click

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const div = document.getElementsByClassName('buttonContainer')[0]
+const btn = document.getElementById('btn')
 
 div.addEventListener('click', (e) =>{
     alert('hola! Soy el div!')
+})
+btn.addEventListener('click', (e) =>{
+    e.stopPropagation()
+    alert('hola!')
 })
